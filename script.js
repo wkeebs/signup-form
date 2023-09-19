9443265cdcdca9d05d67da96eb9b882f1ae12fff
Will Keeble <will.a.keeble@gmail.com>
2023-09-19 14:41:45 +1000

    Add form

---
+++ script.js
@@ -0,0 +1,30 @@
+var password = document.getElementById("password"),
+  confirm_password = document.getElementById("password-confirm"),
+  error_msg = document.getElementById("error-msg");
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    addInvalidPassword();
+  } else {
+    confirm_password.setCustomValidity("");
+    removeInvalidPassword();
+  }
+}
+
+function addInvalidPassword() {
+  password.classList.add("unmatched");
+  confirm_password.classList.add("unmatched");
+
+  error_msg.innerText = "Passwords do not match."
+}
+
+function removeInvalidPassword() {
+  password.classList.remove("unmatched");
+  confirm_password.classList.remove("unmatched");
+
+  error_msg.innerText = ""
+}
+
+password.onchange = validatePassword;
+confirm_password.onchange = validatePassword;
